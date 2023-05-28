406d6fe6b17431ceadc1cfe9f6e3d6617e941203
rzdtjb <rzdtjb@gmail.com>
2023-05-29 03:20:44 +0800

    first

---
+++ src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,39 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.AccountDao;
+import com.itheima.domain.Account;
+import com.itheima.service.AccountService;
+
+import java.util.List;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.itheima.com
+ */
+public class AccountServiceImpl implements AccountService {
+    private AccountDao accountDao;
+
+    public void setAccountDao(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    public void save(Account account) {
+        accountDao.save(account);
+    }
+
+    public void update(Account account){
+        accountDao.update(account);
+    }
+
+    public void delete(Integer id) {
+        accountDao.delete(id);
+    }
+
+    public Account findById(Integer id) {
+        return accountDao.findById(id);
+    }
+
+    public List<Account> findAll() {
+        return accountDao.findAll();
+    }
+}
